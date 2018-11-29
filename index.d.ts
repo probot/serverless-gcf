a2b16b9cd84f7bda168624602384c1d1ba957c6e
Charlie Saunders <charlieasaunders@gmail.com>
2018-11-28 22:21:06 -0500

    Corrects declaration file.
    It seems express doesn't export Request and Response types, so we
    instead import the exported namespace from the type declarations.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module '@probot/serverless-gcf'
 
-import { Request, Response } from 'express'
+import { e } from '@types/express'
 import { ApplicationFunction } from 'probot'
 
-export function serverless(appFn: ApplicationFunction | String): (request: Request, response: Response) => Promise<any>
+export function serverless(appFn: ApplicationFunction | String): (request: e.Request, response: e.Response) => Promise<any>
